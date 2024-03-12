4606246fec0f4654dc059a58ead3ab15dfed15d2
Juan Ticante Vicente <ticantevicentejuan@outlook.com>
2024-03-11 22:12:16 -0600

    add config

---
+++ lib/api_client/api_config.dart
@@ -0,0 +1,7 @@
+abstract class ApiConfig {
+  static const baseUrl = 'https://jsonblob.com/';
+  static const v1 = 'api/';
+  static const contentType = {
+    'Content-Type': 'application/json; chartset=UTF-8'
+  };
+}
---
+++ lib/api_client/path.dart
@@ -0,0 +1,13 @@
+import 'package:http_client/api_client/api_config.dart';
+
+enum Path { dogs }
+
+extension PathExt on Path {
+  String get fullPath {
+    switch (this) {
+      case Path.dogs:
+        const dogos = '1151549092634943488';
+        return ApiConfig.baseUrl + ApiConfig.v1 + dogos;
+    }
+  }
+}
