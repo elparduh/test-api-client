39581c9b531060a57ca838931c09bd827bbda6aa
Juan Ticante Vicente <juan.ticante@justo.mx>
2024-03-13 11:40:54 -0600

    added improvs

--- lib/http_client.dart
+++ lib/http_client.dart
@@ -1,16 +1,15 @@
 import 'package:http_client/api_config/endpoint.dart';
 import 'package:http/http.dart' as http;
 
-abstract class HttpClient {
+/*abstract class HttpClient {
   Future<dynamic> request({
     required Endpoint endpoint,
   });
-}
+}*/
 
-class HttpClientBuilder implements HttpClient {
+class HttpClientBuilder {
   final http.Client client = http.Client();
 
-  @override
   Future request({required Endpoint endpoint}) {
     throw UnimplementedError();
   }
