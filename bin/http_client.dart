ecaf0f6aae7c9758282410e2a5b49387705c6f3a
Juan Ticante Vicente <juan.ticante@justo.mx>
2024-03-13 19:29:49 -0600

    added improvs

--- bin/http_client.dart
+++ bin/http_client.dart
@@ -1,18 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:http_client/api_builder/http_client_builder.dart';
 import 'package:http_client/api_config/endpoint.dart';
 import 'package:http_client/api_config/http_method.dart';
 import 'package:http_client/api_config/path.dart';
 import 'package:http_client/api_models/dogs.dart';
 
 void main(List<String> arguments) {
+  final httpClientBuilder = HttpClientBuilder(http.Client());
   final endpointTest = Endpoint(path: Path.dogs, httpMethod: HttpMethod.get);
 
-  print(endpointTest.getUrlRequest());
-  print(endpointTest.getAllHeaders());
-  print(endpointTest.httpMethod.toString());
-  /*final url = 'https://jsonblob.com/api/1151549092634943488';
-  http.get(Uri.parse(url)).then((res) {
+  final httpResponse = httpClientBuilder.request(endpoint: endpointTest);
+  httpResponse.then((res) {
     final List<dynamic> bodyData = jsonDecode(res.body);
     final List<DogResponse> myDogsParsed = [];
     for (var dog in bodyData) {
@@ -20,5 +19,5 @@ void main(List<String> arguments) {
       myDogsParsed.add(dogItem);
     }
     print(myDogsParsed);
-  });*/
+  });
 }
