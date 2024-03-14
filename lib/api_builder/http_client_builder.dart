0fce85b002394ec7d055755ce09474e349f0af96
Juan Ticante Vicente <juan.ticante@justo.mx>
2024-03-13 20:01:59 -0600

    added improvs

--- lib/api_builder/http_client_builder.dart
+++ lib/api_builder/http_client_builder.dart
@@ -2,12 +2,19 @@ import 'package:http/http.dart' as http;
 import 'package:http_client/api_config/http_method.dart';
 import '../api_config/endpoint.dart';
 
-class HttpClientBuilder {
+abstract class HttpClient {
+  Future<dynamic> request({
+    required Endpoint endpoint,
+  });
+}
+
+class HttpClientBuilder implements HttpClient {
   final http.Client _client;
 
   HttpClientBuilder(this._client);
 
-  Future<dynamic> request({required Endpoint endpoint}) {
+  @override
+  Future request({required Endpoint endpoint}) {
     switch (endpoint.httpMethod) {
       case HttpMethod.get:
         return _client.get(endpoint.getUrlRequest(),
@@ -17,4 +24,15 @@ class HttpClientBuilder {
             headers: endpoint.getAllHeaders());
     }
   }
+
+  /*Future<dynamic> request({required Endpoint endpoint}) {
+    switch (endpoint.httpMethod) {
+      case HttpMethod.get:
+        return _client.get(endpoint.getUrlRequest(),
+            headers: endpoint.getAllHeaders());
+      default:
+        return _client.get(endpoint.getUrlRequest(),
+            headers: endpoint.getAllHeaders());
+    }
+  }*/
 }
--- lib/http_client.dart
+++ lib/http_client.dart
@@ -1,16 +1,1 @@
-import 'package:http_client/api_config/endpoint.dart';
-import 'package:http/http.dart' as http;
 
-/*abstract class HttpClient {
-  Future<dynamic> request({
-    required Endpoint endpoint,
-  });
-}*/
-
-/*class HttpClientBuilder {
-  final http.Client _client = http.Client();
-
-  Future request({required Endpoint endpoint}) {
-    throw UnimplementedError();
-  }
-}*/
